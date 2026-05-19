8a471c9d6a730afb14d1e44bde58ab8ff3d263c9
sanshamilova <san.shamilova@gmail.com>
2026-05-19 16:01:35 +0300

    Загружен код к 1 заданию

--- 1.py
+++ 1.py
@@ -1,9 +1,10 @@
 import ifcopenshell 
 
-
+#Путь к файлу
 file_path = r"E:\SnakePupi\python_labor\Example_1.ifc"
 
 model = ifcopenshell.open(file_path)
 
+#Количество стен
 walls = model.by_type("IfcWall")
 print("Число стен:", len(walls))
